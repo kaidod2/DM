5bbf24c8f2ca90b3fbbc87a9fe1b796448563b46
edu46 <jewoos2921@naver.com>
2024-10-25 11:12:00 +0900

    chapter9 작성중 -2

--- src/OnlinePlanning.py
+++ src/OnlinePlanning.py
@@ -57,3 +57,119 @@ class ForwardSearch(OnlinePlanningMethod):
     def __call__(self, s: Any) -> Any:
         return forward_search(self.P, s, self.d, self.U)[0]
 
+
+def branch_and_bound(P: MDP, s: Any, d: int,
+                     U_lo: Callable[[Any], float],
+                     Q_hi: Callable[[Any, Any], float]) -> tuple[Any, float]:
+    if d <= 0:
+        return None, U_lo(s),
+
+    def U_prime(s):
+        return branch_and_bound(P, s, d - 1, U_lo, Q_hi)[1]
+
+    best_a, best_u = (None, -np.inf)
+    for a in sorted(P.A, key=(lambda a: Q_hi(s, a)), reverse=True):
+        if Q_hi(s, a) < best_u:
+            return best_a, best_u
+        u = P.lookahead(U_prime, s, a)
+        if u > best_u:
+            best_a, best_u = a, u
+    return best_a, best_u
+
+
+class BranchAndBound(OnlinePlanningMethod):
+    def __init__(self, P: MDP, d: int,
+                 U_lo: Callable[[Any], float],
+                 Q_hi: Callable[[Any, Any], float]):
+        self.P = P  # problem
+        self.d = d  # depth
+        self.U_lo = U_lo  # lower bound on value function at depth d
+        self.Q_hi = Q_hi  # upper bound on action value function
+
+    def __call__(self, s: Any) -> Any:
+        return branch_and_bound(self.P, s, self.d, self.U_lo, self.Q_hi)[0]
+
+
+def sparse_sampling(P: MDP, s: Any, d: int, m: int, U: Callable[[Any], float]):
+    if d <= 0:
+        return None, U(s)
+    best_a, best_u = (None, -np.inf)
+
+    for a in P.A:
+        u = 0.0
+        for _ in range(m):
+            s_prime, r = P.randstep(s, a)
+            a_prime, u_prime = sparse_sampling(P, s_prime, d - 1, m, U)
+            u += (r + P.gamma * u_prime) / m
+
+        if u > best_u:
+            best_a, best_u = a, u
+    return best_a, best_u
+
+
+class SparseSampling(OnlinePlanningMethod):
+    def __init__(self, P: MDP, d: int, m: int, U: Callable[[Any], float]):
+        self.P = P
+        self.d = d
+        self.m = m  # number of samples
+        self.U = U  # value function at depth d
+
+    def __call__(self, s: Any) -> Any:
+        return sparse_sampling(self.P, s, self.d, self.m, self.U)[0]
+
+
+class MonteCarloTreeSearch(OnlinePlanningMethod):
+    def __init__(self,
+                 P: MDP,
+                 N: dict[tuple[Any, Any], int],
+                 Q: dict[tuple[Any, Any], float],
+                 d: int,
+                 m: int,
+                 c: float,
+                 U: Callable[[Any], float]):
+        self.P = P  # problem
+        self.N = N  # visit counts
+        self.Q = Q  # action value estimates
+        self.d = d  # depth
+        self.m = m  # number of simulations
+        self.c = c  # exploration constant
+        self.U = U  # value function estimate
+
+    def __call__(self, s: Any) -> Any:
+        for _ in range(self.m):
+            self.simulate(s, d=self.d)
+        return self.P.A[np.argmax([self.Q[(s, a)] for a in self.P.A])]
+
+    def simulate(self, s: Any, d: int):
+        if d <= 0: return self.U(s)
+
+        if (s, self.P.A[0]) not in self.N:
+            for a in self.P.A:
+                self.N[(s, a)] = 0
+                self.Q[(s, a)] = 0.0
+        a = self.explore(s)
+        s_prime, r = self.P.randstep(s, a)
+        q = r + self.P.gamma * self.simulate(s_prime, d - 1)
+        self.N[(s, a)] += 1
+        self.Q[(s, a)] += (q - self.Q[(s, a)]) / self.N[(s, a)]
+        return q
+
+    def explore(self, s: Any) -> Any:
+        A, N = self.P.A, self.N
+        Ns = np.sum([N[(s, a)] for a in A])
+        return A[np.argmax([self.ucb1(s, a, Ns) for a in A])]
+
+    def ucb1(self, s: Any, a: Any, Ns: int) -> float:
+        N, Q, c = self.N, self.Q, self.c
+        return Q[(s, a)] + c * self.bonus(N[(s, a)], Ns)
+
+    @staticmethod
+    def bonus(Nsa: int, Ns: int) -> float:
+        return np.inf if Nsa == 0 else np.sqrt(np.log(Ns) / Nsa)
+
+
+class HeuristicSearch(OnlinePlanningMethod):
+    def __init__(self, P: MDP, U_hi: Callable[[Any], float],
+                 d: int, m: int):
+        self.P = P
+        self.U_hi = U_hi
